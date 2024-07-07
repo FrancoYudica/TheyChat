84ce181c8c6389b4f73ac90ee50bd2eb246f1508
Franco Yudica <francoyudica@gmail.com>
2024-07-07 12:43:29 -0300

    Removed input handler data typedef and changed to static variables

--- include/client/ui/input_handler.h
+++ include/client/ui/input_handler.h
@@ -1,23 +1,6 @@
 #ifndef __INPUT_HANDLER_H__
 #define __INPUT_HANDLER_H__
 #include "they_chat_error.h"
-#include "pthread.h"
-
-typedef struct
-{
-    // Input callback, called after user entered input.
-    Error* (*input_callback)(const char*);
-
-    // Input callback, called after user entered a command.
-    Error* (*command_callback)(const char*);
-    void* user_data;
-    Error* error;
-    pthread_t input_thread;
-
-    /// @brief Mutex used to block setters and getter on input functions
-    pthread_mutex_t mutex;
-
-} InputHandlerData;
 
 void input_handler_init();
 void input_handler_free();
--- src/client/ui/input_handler.c
+++ src/client/ui/input_handler.c
@@ -1,12 +1,22 @@
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "ui/input_handler.h"
 #include "ui/ui.h"
 #include "string/utils.h"
-#include "pthread.h"
 #include "messages/message_types.h"
 
-static InputHandlerData s_input_data;
+// Input callback, called after user entered input.
+static Error* (*s_input_callback)(const char*);
+
+// Input callback, called after user entered a command.
+static Error* (*s_command_callback)(const char*);
+
+static Error* s_err;
+static pthread_t s_input_thread;
+
+/// @brief Mutex used to block setters and getter on input functions
+static pthread_mutex_t s_mutex;
 
 /// @brief Flag used to tell input thread when application finishes
 static bool s_running = false;
@@ -28,24 +38,24 @@ static void* handle_input()
         char* trimmed_input = trim(input);
         bool is_command = trimmed_input[0] == '/' && strlen(trimmed_input) > 1;
 
-        Error* err = CREATE_ERR_OK;
-
         // Locks when calling callback
-        pthread_mutex_lock(&s_input_data.mutex);
+        pthread_mutex_lock(&s_mutex);
 
         // Command callback
-        if (is_command && s_input_data.command_callback != NULL) {
-            err = s_input_data.command_callback(input);
-        }
-        // Usual input callback
-        else if (s_input_data.input_callback != NULL) {
-            err = s_input_data.input_callback(input);
-        }
-        pthread_mutex_unlock(&s_input_data.mutex);
+        if (is_command && s_command_callback != NULL)
+            s_err = s_command_callback(input);
 
-        if (IS_NET_ERROR(err)) {
-            s_input_data.error = err;
-            break;
+        // Usual input callback
+        else if (s_input_callback != NULL)
+            s_err = s_input_callback(input);
+
+        pthread_mutex_unlock(&s_mutex);
+
+        if (IS_NET_ERROR(s_err)) {
+            ui_push_text_entry(
+                TEXT_ENTRY_TYPE_WARNING,
+                "Error ocurred in handle_input thread \"%s\"",
+                s_err->message);
         }
         usleep(50000); // Sleep for 5ms to prevent high CPU usage
     }
@@ -56,30 +66,28 @@ static void* handle_input()
 void input_handler_init()
 {
     s_running = true;
-    s_input_data.input_callback = NULL;
-    s_input_data.user_data = NULL;
-    s_input_data.error = NULL;
-    pthread_create(&s_input_data.input_thread, NULL, handle_input, NULL);
-    pthread_mutex_init(&s_input_data.mutex, NULL);
+    s_input_callback = NULL;
+    pthread_create(&s_input_thread, NULL, handle_input, NULL);
+    pthread_mutex_init(&s_mutex, NULL);
 }
 
 void input_handler_free()
 {
     s_running = false;
-    pthread_detach(s_input_data.input_thread);
-    pthread_mutex_destroy(&s_input_data.mutex);
+    pthread_detach(s_input_thread);
+    pthread_mutex_destroy(&s_mutex);
 }
 
 void input_handler_set_input_callback(Error* (*input_callback)(const char*))
 {
-    pthread_mutex_lock(&s_input_data.mutex);
-    s_input_data.input_callback = input_callback;
-    pthread_mutex_unlock(&s_input_data.mutex);
+    pthread_mutex_lock(&s_mutex);
+    s_input_callback = input_callback;
+    pthread_mutex_unlock(&s_mutex);
 }
 
 void input_handler_set_command_callback(Error* (*input_callback)(const char*))
 {
-    pthread_mutex_lock(&s_input_data.mutex);
-    s_input_data.command_callback = input_callback;
-    pthread_mutex_unlock(&s_input_data.mutex);
+    pthread_mutex_lock(&s_mutex);
+    s_command_callback = input_callback;
+    pthread_mutex_unlock(&s_mutex);
 }
